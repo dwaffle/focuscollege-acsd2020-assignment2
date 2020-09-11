6f76d09f46e66bdabd2731bda36161a7ec12722b
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-09-11 13:19:08 -0700

    Added calculations for a single wall and the entire house.

--- src/BoardCalculator.ts
+++ src/BoardCalculator.ts
@@ -6,6 +6,9 @@ const postSize = 3.5;
 let distanceBetweenBoards = 16;
 const twoByFourLength = 96; //Eight feet in inches.
 
+//Tracks extra posts needed
+let extraPosts = 0;
+
 //Calculate how many additional posts are needed.
 function CalculateAdditionalPostsNeeded(outerWallWidthInFeet:number)
 {
@@ -16,14 +19,14 @@ function CalculateAdditionalPostsNeeded(outerWallWidthInFeet:number)
 //Calculates inner wall width.
 function CalculateInnerWallWidth(outerWallWidthInFeet:number)
 {
-    let extraPosts = CalculateAdditionalPostsNeeded(outerWallWidthInFeet);
+    extraPosts = CalculateAdditionalPostsNeeded(outerWallWidthInFeet);
     //Accout for normal posts, then take out the extra posts if needed.  If not needed, extraPosts will be 0 * 3.5.
     let innerWallWidth = (outerWallWidthInFeet * 12) - (postSize * 2) - (extraPosts * postSize);  
     return innerWallWidth;
 }
 
 //Calculate boards required for inner wall.
-function CalculateBoardsRequiredForInnerWall(outerWallWidthInFeet:number)
+function CalculateInnerWallBoards(outerWallWidthInFeet:number)
 {
     let totalBoards = CalculateInnerWallWidth(outerWallWidthInFeet)/16;
     return totalBoards;
@@ -40,4 +43,21 @@ function CalculateFloorBoards(outerWallWidthInFeet:number)
 function CalculateCeilingBoards(outerWallWidthInFeet:number)
 {
     return CalculateFloorBoards(outerWallWidthInFeet);
+}
+
+//Calculate total boards needed for one wall
+function CalculateWall(outerWallWidthInFeet:number)
+{
+    let totalBoardsNeeded = CalculateFloorBoards(outerWallWidthInFeet)
+    + CalculateCeilingBoards(outerWallWidthInFeet)
+    + CalculateInnerWallBoards(outerWallWidthInFeet);
+    return totalBoardsNeeded;
+}
+
+//Calculate total boards needed for house.
+function AddUpBoards(lengthOfHouseInFeet:number, widthOfHouseInFeet:number)
+{
+   let finishedHouse = CalculateWall(lengthOfHouseInFeet) * 2 + CalculateWall(widthOfHouseInFeet) * 2;
+   console.log("The house requires " + finishedHouse + " boards");
+   console.log("It also requires " + (extraPosts + 4) + " posts.");
 }

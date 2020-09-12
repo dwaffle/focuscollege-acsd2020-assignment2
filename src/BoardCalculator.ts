c0510070421ff55a36717700520873571578403c
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-09-12 13:30:16 -0700

    Added additional comments.

--- src/BoardCalculator.ts
+++ src/BoardCalculator.ts
@@ -1,10 +1,9 @@
 //Sizes of wood in inches.
-const twoByFourWidth = 3.5;
 const postSize = 3.5;
 const twoByFourLength = 96; //Eight feet in inches.
 
 //Gerald's numbers
-let distanceBetweenBoards = 16;
+const distanceBetweenBoards = 16;
 
 //How many boards are needed on floor and roof?
 function CalculateFloorAndRoofBoards(outerWallWidthInFeet:number)
@@ -48,14 +47,17 @@ function CalcTotalBoards(longWallWidthInFeet:number, wideWallWidthInFeet:number)
    console.log("Additional wide wall posts: " + extraWidePosts);
    let totalPosts = extraLongPosts + extraWidePosts;
    //Two extra boards are needed for each extra post, as Gerald only places posts at the end of a full board
-   //meaning he needs to only make two additional cuts.
+   //meaning he needs to only make two additional cuts. Does not deal with the possible corner case
+   //of a multiple of eight feet on both sides of a post (would require a minimum gap of 24 feet and 3.5 inches?)
+   //In that case, he would not need to make a cut.
    let totalBoards = longWallBoards + wideWallBoards + (totalPosts * 2);
    return {
        totalBoards: totalBoards, 
        totalPosts: totalPosts
     };
 }
-//Account for waste.
+
+//Add 10% rounded up to account for waste.
 function AccountForWaste(totalBoards:number)
 {
     return Math.ceil(totalBoards * 1.1);
@@ -68,5 +70,6 @@ export default function BuildHouse(longWallWidthInFeet:number, wideWallWidthInFe
     let totalBoardsPlusWaste = AccountForWaste(boardsAndPosts.totalBoards);
     let totalPostsPlusWaste = AccountForWaste(boardsAndPosts.totalPosts);
     console.log("Total boards to purchase after accounting for waste: " + totalBoardsPlusWaste);
-    console.log("Total posts to purchase after accounting for waste: " + (Number(totalPostsPlusWaste) + 4));  //Account for four posts in every house.
+    //+4 accounts for the four posts that start in every house.
+    console.log("Total posts to purchase after accounting for waste: " + (Number(totalPostsPlusWaste) + 4));
 }
